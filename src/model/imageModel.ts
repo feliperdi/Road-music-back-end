75d28f6b13043614df2ac71a153085db49230b42
feliperdi <dias.rodrigues.feliphe@gmail.com>
2020-10-12 15:48:31 -0300

    music and image models

---
+++ src/model/imageModel.ts
@@ -0,0 +1,9 @@
+export default interface imageModel{
+    id: string,
+    subtitle: string,
+    author: string,
+    date: Date,
+    file: string,
+    tags: string[],
+    collections: string
+}
---
+++ src/model/musicModel.ts
@@ -0,0 +1,9 @@
+export default interface musicModel{
+    id: string,
+    title: string,
+    author: string,
+    date: Date,
+    file: string,
+    genre: string[],
+    album: string
+}
